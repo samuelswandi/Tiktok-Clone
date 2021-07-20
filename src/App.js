2bb659907df204c59e8cce4b9f6bbee66092286c
Samuel Swandi <samuel.swandi@gmail.com>
2021-07-20 17:24:15 +0700

    finish build

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
+import axios from './axios.js';
 import './App.css';
 import Video from './Video'
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('v2/posts')
+      setVideos(response.data);
+
+      return response;
+    }
+
+    fetchPosts();
+  }, [])
+
+  console.log(videos)
+  // console.log()
   return (
     <div className="App">
       <div className="app__videos">
-        <Video url = "test.mp4"
-        username = "orang random"
-        description = "this is a description" 
-        song = "hahay palpale nyaw"
-        likes = {100}
-        comments = {100}
-        shares = {10}
-        />
-        <Video url = "test2.mp4"
-        username = "sem"
-        description = "love u <3" 
-        song = "love gracieeeeeeeeee"
-        likes = {1000}
-        comments = {1000}
-        shares = {1000}
-        />
-        <Video url = "test3.mp4"
-        username = "azka"
-        description = "aish nice gg" 
-        song = ":V :V"
-        likes = {100}
-        comments = {100}
-        shares = {100}
-        />
-        <Video url = "test4.mp4"
-        username = "epi"
-        description = "bekantan lari" 
-        song = "ada hewan bekantan"
-        likes = {100}
-        comments = {10}
-        shares = {100}
-        />
+        {videos.map(
+          ({ url, username, description, song, likes, shares, comments }) => {
+            return(
+            <Video
+              url={url}
+              username={username}
+              description={description}
+              song={song}
+              likes={likes}
+              shares={shares}
+              comments={comments}
+            />)
+          })}
+
       </div>
     </div>
 
